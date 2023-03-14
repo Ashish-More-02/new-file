28ac0f67837728ac8c0fc76a1a02a750d18f39e3
Ashish-More-02 <ashishmore2125@gmail.com>
2023-03-14 23:38:53 +0530

    this is modified file

--- new.py
+++ new.py
@@ -9,3 +9,20 @@ sum = num1 + num2
 
 # printing values
 print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+
+
+# this file is now modified and it have to be commited agian lets see how to do it 
+
+x=20 
+y=30 
+
+if (x>20):{
+        print ("x is greater than 20 ")
+}
+elif(x<20):{
+        print("x is less than 20")
+}
+else:{
+        print("hello world , everyting is ok !")
+}
+
